5d73eed03ccfaa8b73d55a4d2bf1d7907a3c0c7b
Russell Hayes <info@rahphotographyco.com>
2017-12-07 22:05:45 -0700

    Added input to app.js

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Hero from './components/Hero';
 import LotsOfGreetings from './components/LotsOfGreetings';
 import BlinkApp from './components/BlinkApp';
+import Input from './components/Input';
 
 export default class App extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
         <Hero/>
+        <Input/>
         <LotsOfGreetings/>
         <BlinkApp/>
       </ScrollView>
